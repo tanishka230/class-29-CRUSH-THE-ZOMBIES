17c034d4f573fb7e04c0f8b3a807d5fd73fd603a
tanishka230 <84577096+tanishka230@users.noreply.github.com>
2021-10-25 12:03:13 +0530

    Add files via upload

---
+++ link.js
@@ -0,0 +1,12 @@
+class Link{
+    constructor(bodyA,bodyB){
+        var lastLink=bodyA.body.bodies.length-2;
+        this.link =Constraint.create({
+            bodyA:bodyA.body.bodies[lastLink],
+            bodyB:bodyB,
+            length:-10,
+            stiffness:0.01
+        })
+        World.add(world,this.link);
+    }
+}
--- sketch.js
+++ sketch.js
@@ -9,65 +9,45 @@ const Composite = Matter.Composite;
 
 let engine;
 let world;
-var ground, bridge;
-var leftWall, rightWall;
-var jointPoint;
-var jointLink;
+var ground;
+var rope;
+var fruit;
+var fruit_con;
 
-var stones = [];
-
-function setup() {
-  createCanvas(windowWidth, windowHeight);
+function setup() 
+{
+  createCanvas(500,700);
+  frameRate(80);
   engine = Engine.create();
   world = engine.world;
-  frameRate(80);
+  ground = new Ground(200,680,600,20);
+  rope=new Rope(8,{x:250,y:30})
 
-  ground = new Base(0, height - 10, width * 2, 20, "#795548", true);
-  leftWall = new Base(300, height / 2 + 50, 600, 100, "#8d6e63", true);
-  rightWall = new Base(width - 300, height / 2 + 50, 600, 100, "#8d6e63", true);
-
-  /*bridge = new Base(15, { x: width / 2 - 400, y: height / 2 });
-  jointPoint = new Base(width - 600, height / 2 + 10, 40, 20, "#8d6e63", true);*/
-
-  /*bridge = new Bridge(15, { x: width / 2 - 400, y: height / 2 });
-  jointPoint = new Base(width - 600, height / 2 + 10, 40, 20, "#8d6e63", true);*/
-
-  /*bridge = new Base(15, { x: width / 2 - 400, y: height / 2 });
-  jointPoint = new Bridge(width - 600, height / 2 + 10, 40, 20, "#8d6e63", true);*/
-
-  /*bridge = new Bridge(15, { x: width / 2 - 400, y: height / 2 });
-  jointPoint = new Bridge(width - 600, height / 2 + 10, 40, 20, "#8d6e63", true);*/
-
-  
-  //Matter.Composite.add(bridge.body, jointPoint);
-
-  //Matter.Composite.add(jointPoint);
-  
-  //Matter.Composite.add(jointPoint, bridge.body);
-  
-  //Matter.Composite.add(bridge.body);
-
-
-  jointLink = new Link(bridge, jointPoint);
-
-  for (var i = 0; i <= 8; i++) {
-    var x = random(width / 2 - 200, width / 2 + 300);
-    var y = random(-10, 140);
-    var stone = new Stone(x, y, 80, 80);
-    stones.push(stone);
+  var fruit_options={
+    density:0.001
   }
+
+  fruit=Bodies.circle(300,300,20,fruit_options);
+  Matter.Composite.add(rope.body,fruit);
+
+  fruit_con=new Link(rope,fruit);
+
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+  textSize(50)
+  
 }
 
-function draw() {
+function draw() 
+{
   background(51);
-  Engine.update(engine);
-
   ground.show();
-  bridge.show();
-  leftWall.show();
-  rightWall.show();
+  rope.show();
+  ellipse(fruit.position.x,fruit.position.y,20,20);
+  
+  Engine.update(engine);
+  
 
-  for (var stone of stones) {
-    stone.show();
-  }
+ 
+   
 }
